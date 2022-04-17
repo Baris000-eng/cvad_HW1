971156dd68351cc39fb10cb90ae2b80f59984363
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-04-17 18:57:31 +0000

    abbbc

--- cvad_hw1/models/affordance_predictor.py
+++ cvad_hw1/models/affordance_predictor.py
@@ -4,7 +4,10 @@ import torch.nn as nn
 class AffordancePredictor(nn.Module):
     """Afforance prediction network that takes images as input"""
     def __init__(self):
+        super(AffordancePredictor,self).__init__()
+       
         pass
 
     def forward(self, img):
+        
         pass
