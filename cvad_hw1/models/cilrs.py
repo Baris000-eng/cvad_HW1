90d11a66a4eb2a5f17a89e6ce25f482b872b40c8
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-04-17 19:05:37 +0000

    myc

--- cvad_hw1/models/cilrs.py
+++ cvad_hw1/models/cilrs.py
@@ -1,9 +1,13 @@
+import torchvision.models as models
 import torch.nn as nn
 
 
 class CILRS(nn.Module):
     """An imitation learning agent with a resnet backbone."""
     def __init__(self):
+      super(CILRS,self).__init__()
+      my_resnet_model = models.resnet18(pretrained=True)
+      
         
 
     def forward(self, img, command):
