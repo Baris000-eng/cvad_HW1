a4c6a4d15819134a65ed03617c216abe4558f8d7
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-04-17 19:12:39 +0000

    newcom

--- cvad_hw1/models/cilrs.py
+++ cvad_hw1/models/cilrs.py
@@ -6,7 +6,8 @@ class CILRS(nn.Module):
     """An imitation learning agent with a resnet backbone."""
     def __init__(self):
       super(CILRS,self).__init__()
-      my_resnet_model = models.resnet18(pretrained=True)
+      is_model_pretrained = True
+      my_resnet_model = models.resnet18(pretrained=is_model_pretrained)
       
         
 
