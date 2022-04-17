82c9942c74c74acba6b57dd60f63997086486834
Baris000eng <63741832+Baris000-eng@users.noreply.github.com>
2022-04-17 16:13:41 +0000

    abc

--- cvad_hw1/models/cilrs.py
+++ cvad_hw1/models/cilrs.py
@@ -3,8 +3,8 @@ import torch.nn as nn
 
 class CILRS(nn.Module):
     """An imitation learning agent with a resnet backbone."""
-    def __init__(self):
-       
+   def __init__(self):
+        pass
 
     def forward(self, img, command):
         pass
